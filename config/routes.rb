70844c9778685b012e5c76c572126c5025aa3dbc
Sandy <SVern99@aol.com>
2017-04-07 13:07:11 +0000

    Added nav and footer partials and edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   root to: 'restaurants#index'
-  get 'pages/about', to: 'pages#about'
-  get 'pages/contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
