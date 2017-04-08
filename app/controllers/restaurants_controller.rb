b85d2713b33dfe6e47badc30fd2849efe5464517
Sandy <SVern99@aol.com>
2017-04-08 17:43:24 +0000

    Added new restaurant form, updated routes, and added actions to controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,15 @@
 class RestaurantsController < ApplicationController
   def index
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  resources :restaurants
+
   root to: 'restaurants#index'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
