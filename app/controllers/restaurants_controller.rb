b0238bf8dc66eaf0c2894314885423e99e0050ac
Sandy <SVern99@aol.com>
2017-04-10 21:32:55 +0000

    Added restaurant parameters and show action and view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,9 +7,22 @@ class RestaurantsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to @restaurant
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description, :address1, :address2,
+    :city, :state_provence, :postalcode, :phone, :email, :category_id, :image)
   end
 
 end
