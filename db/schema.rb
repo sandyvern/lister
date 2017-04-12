8fc1b330ea7370a89b87e38d1f2f317b5608673f
Sandy <SVern99@aol.com>
2017-04-12 18:12:28 +0000

    Added category_id to restaurants, edited form, added category_id to whitelist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403203121) do
+ActiveRecord::Schema.define(version: 20170410233322) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20170403203121) do
     t.string   "email"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_restaurants_on_category_id"
   end
 
   create_table "reviews", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(name: 'Fast Food')
+Category.create(name: 'Seafood')
+Category.create(name: 'Steak House')
+
