afffda3bb9a8cbd68699ee8b9aa249a871b16004
Sandy <SVern99@aol.com>
2017-04-13 22:50:13 +0000

    Nested review resources, added review associations, added new and create actions to review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,34 @@
 class ReviewsController < ApplicationController
+  before_action :set_the_restaurant
+  before_action :authenticate_user!
+
   def index
+  end
+
+  def new
+    @review = Review.new(restaurant: @restaurant)
+  end
+
+  def create
+  @user = current_user
+  @review = current_user.reviews.build(review_params)
+  @review.restaurant = @restaurant
+    if @review.save
+      flash[:success] = "Thanks #{@user.username}, Your review has been added"
+      redirect_to @restaurant
+    else
+      flash[:error] = "Oops #{@user.username}, there seems to be a problem"
+      render :new
+    end
+  end
+
+  private
+
+  def set_the_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
   end
 end
