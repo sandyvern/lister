ed7f13566ff7207b942c6e79b1d1d257a32adb91
Sandy <SVern99@aol.com>
2017-04-13 21:57:41 +0000

    Added user_id and restaurant_id to reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410233322) do
+ActiveRecord::Schema.define(version: 20170413215205) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -37,8 +37,12 @@ ActiveRecord::Schema.define(version: 20170410233322) do
   create_table "reviews", force: :cascade do |t|
     t.string   "comment"
     t.integer  "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "user_id"
+    t.integer  "restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
