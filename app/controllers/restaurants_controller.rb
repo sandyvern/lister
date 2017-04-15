b5b1195327ee3c0dd14e96672bafc82d700f32de
Sandy <SVern99@aol.com>
2017-04-15 19:25:17 +0000

    Added authentication

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -22,6 +22,7 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @reviews = Review.where(restaurant_id: @restaurant)
   end
 
   def update
