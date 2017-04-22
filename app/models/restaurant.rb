ef5f7de186b7f1c468a4156ac8e66697dd8b0bad
Sandy <SVern99@aol.com>
2017-04-22 17:32:32 +0000

    Added seeds file

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,11 @@ class Restaurant < ApplicationRecord
   belongs_to :category
 
   validates_presence_of  :name, :description, :city, :state_provence
+
+  geocoded_by :full_address
+  after_validation :geocode
+
+  def full_address
+    [city, state_provence].join(', ')
+  end
 end
